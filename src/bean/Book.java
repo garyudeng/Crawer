f42959ee72743403e4056b474d58ebcdf2b53a0f
sunwubin <sun@sun-PC>
2013-07-30 09:24:52 +0800

    add

---
+++ src/bean/Book.java
@@ -0,0 +1,96 @@
+package bean;
+
+public class Book {
+	private int id;
+	private String uuId;
+	private String bookName;
+	private String author;
+	private String translAtors;
+	private String press;
+	private String version;
+	private String isbn;
+	private String outline;
+	private Integer wantRead;
+	private Integer reading;
+	private Integer read;
+	private String directory;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUuId() {
+		return uuId;
+	}
+	public void setUuId(String uuId) {
+		this.uuId = uuId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTranslAtors() {
+		return translAtors;
+	}
+	public void setTranslAtors(String translAtors) {
+		this.translAtors = translAtors;
+	}
+	public String getPress() {
+		return press;
+	}
+	public void setPress(String press) {
+		this.press = press;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getOutline() {
+		return outline;
+	}
+	public void setOutline(String outline) {
+		this.outline = outline;
+	}
+	public Integer getWantRead() {
+		return wantRead;
+	}
+	public void setWantRead(Integer wantRead) {
+		this.wantRead = wantRead;
+	}
+	public Integer getReading() {
+		return reading;
+	}
+	public void setReading(Integer reading) {
+		this.reading = reading;
+	}
+	public Integer getRead() {
+		return read;
+	}
+	public void setRead(Integer read) {
+		this.read = read;
+	}
+	public String getDirectory() {
+		return directory;
+	}
+	public void setDirectory(String directory) {
+		this.directory = directory;
+	}
+
+}
